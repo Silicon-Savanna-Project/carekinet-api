2e0a1be8ee2d3ceff633179bdce7cbb9dad5dc07
ger619 <draker74@gmail.com>
2023-08-31 11:27:40 +0300

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  # def update_allowed_parameters
+  #  devise_parameter_sanitizer.permit(:sign_up) do |u|
+  #    u.permit(:first_name, :last_name, :role, :email, :password, :photo_id, :id_number, :phone_number)
+  #  end
+  #  devise_parameter_sanitizer.permit(:account_update) do |u|
+  #    u.permit(:first_name, :last_name, :role, :email, :password, :current_password, :photo_id, :id_number,
+  #             :phone_number)
+  #  end
+  # end
 end
